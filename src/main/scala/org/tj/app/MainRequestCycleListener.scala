010571d919e7103a866440b20d3462e88b23a773
gtukmachev <gtukmachev@gmail.com>
2015-09-23 18:02:08 +0600

    add Hib utils functions

---
+++ src/main/scala/org/tj/app/MainRequestCycleListener.scala
@@ -0,0 +1,32 @@
+package org.tj.app
+
+import org.apache.wicket.request.cycle.{IRequestCycleListener, RequestCycle}
+import org.apache.wicket.request.{IRequestHandler, Url}
+import org.tj.hibernate.Hib
+
+/**
+ * Created by Judge on 23.09.2015.
+ */
+class MainRequestCycleListener extends IRequestCycleListener{
+
+  override def onExceptionRequestHandlerResolved(cycle: RequestCycle, handler: IRequestHandler, exception: Exception): Unit = {}
+
+  override def onRequestHandlerResolved(cycle: RequestCycle, handler: IRequestHandler): Unit = {}
+
+  override def onDetach(cycle: RequestCycle): Unit = {}
+
+  override def onRequestHandlerScheduled(cycle: RequestCycle, handler: IRequestHandler): Unit = {}
+
+  override def onBeginRequest(cycle: RequestCycle): Unit = {}
+
+  override def onException(cycle: RequestCycle, ex: Exception): IRequestHandler = null
+
+  override def onUrlMapped(cycle: RequestCycle, handler: IRequestHandler, url: Url): Unit = {}
+
+  override def onRequestHandlerExecuted(cycle: RequestCycle, handler: IRequestHandler): Unit = {}
+
+  override def onEndRequest(cycle: RequestCycle): Unit = {
+    // commit current transaction (if have one)
+    Hib.close(true)
+  }
+}
--- src/main/scala/org/tj/hibernate/Hib.scala
+++ src/main/scala/org/tj/hibernate/Hib.scala
@@ -1,11 +1,13 @@
 package org.tj.hibernate
 
-import org.hibernate.SessionFactory
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder
 import org.hibernate.boot.{Metadata, MetadataSources}
+import org.hibernate.resource.transaction.spi.TransactionStatus
+import org.hibernate.{Session, SessionFactory}
 
 /**
  * Created by Judge on 22.09.2015.
+ *
  */
 object Hib  {
 
@@ -37,5 +39,56 @@ object Hib  {
 
   }
 
+  def destroyHib() = {
+    close(true)
+    sessionFactory.close()
+  }
+
+  def get :Session = {
+
+    var session = sessionFactory.getCurrentSession
+
+    if (session == null) {
+      session = sessionFactory.openSession()
+      session.beginTransaction()
+    }
+
+    session
+  }
+
+  def commit() = {
+    val localThreadSession = sessionFactory.getCurrentSession
+    if (localThreadSession != null) {
+      val transaction = localThreadSession.getTransaction
+      if (transaction != null) {
+        if (transaction.getStatus.isNotOneOf(TransactionStatus.ACTIVE) ) {
+          transaction.commit()
+          localThreadSession.beginTransaction()
+        }
+      }
+    }
+  }
+
+  def close(isCommitTransaction :Boolean) :Unit = {
+    val localThreadSession = sessionFactory.getCurrentSession
+    if (localThreadSession != null) {
+
+      try {
+        val transaction = localThreadSession.getTransaction
+        if (transaction != null) {
+          if (transaction.getStatus.isNotOneOf(TransactionStatus.ACTIVE) ) {
+            if (isCommitTransaction) {
+              transaction.commit()
+            } else {
+              transaction.rollback()
+            }
+          }
+        }
+      } finally {
+        localThreadSession.close()
+      }
+
+    }
+  }
 
 }
